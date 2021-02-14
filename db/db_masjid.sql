c3a8e498b34c38065fb215735d6b96f091cea873
mastukino <mas.kino@gmail.com>
2021-02-14 20:50:01 +0700

    first commit

---
+++ db/db_masjid.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 20, 2020 at 06:49 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_masjid`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kas_masjid`
+--
+
+CREATE TABLE `kas_masjid` (
+  `id_km` int(11) NOT NULL,
+  `tgl_km` varchar(50) DEFAULT NULL,
+  `uraian_km` varchar(50) DEFAULT NULL,
+  `masuk` varchar(50) DEFAULT NULL,
+  `keluar` varchar(50) DEFAULT NULL,
+  `jenis` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kas_masjid`
+--
+
+INSERT INTO `kas_masjid` (`id_km`, `tgl_km`, `uraian_km`, `masuk`, `keluar`, `jenis`) VALUES
+(1, '2020-05-20', 'Hamba Allah', '2000000', '0', 'Masuk'),
+(2, '2020-05-20', 'Bayar Imam Masjid', '0', '500000', 'Keluar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kas_sosial`
+--
+
+CREATE TABLE `kas_sosial` (
+  `id_km` int(11) NOT NULL,
+  `tgl_ks` varchar(50) DEFAULT NULL,
+  `uraian_ks` varchar(50) DEFAULT NULL,
+  `masuk` varchar(50) DEFAULT NULL,
+  `keluar` varchar(50) DEFAULT NULL,
+  `jenis` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kas_sosial`
+--
+
+INSERT INTO `kas_sosial` (`id_km`, `tgl_ks`, `uraian_ks`, `masuk`, `keluar`, `jenis`) VALUES
+(1, '2020-05-20', 'hamba allah', '4000000', '0', 'Masuk'),
+(2, '2020-05-20', 'belikarpet', '0', '200000', 'Keluar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pengguna`
+--
+
+CREATE TABLE `tb_pengguna` (
+  `id_pengguna` int(11) NOT NULL,
+  `nama_pengguna` varchar(50) NOT NULL DEFAULT '0',
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `level` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_pengguna`
+--
+
+INSERT INTO `tb_pengguna` (`id_pengguna`, `nama_pengguna`, `username`, `password`, `level`) VALUES
+(1, 'Syahril Effendi', 'syahril', 'test123', 'Administrator'),
+(2, 'Amrina Rasyada', 'rina', '123', 'Bendahara');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kas_masjid`
+--
+ALTER TABLE `kas_masjid`
+  ADD KEY `ID` (`id_km`);
+
+--
+-- Indexes for table `kas_sosial`
+--
+ALTER TABLE `kas_sosial`
+  ADD KEY `ID` (`id_km`);
+
+--
+-- Indexes for table `tb_pengguna`
+--
+ALTER TABLE `tb_pengguna`
+  ADD PRIMARY KEY (`id_pengguna`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kas_masjid`
+--
+ALTER TABLE `kas_masjid`
+  MODIFY `id_km` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `kas_sosial`
+--
+ALTER TABLE `kas_sosial`
+  MODIFY `id_km` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tb_pengguna`
+--
+ALTER TABLE `tb_pengguna`
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
